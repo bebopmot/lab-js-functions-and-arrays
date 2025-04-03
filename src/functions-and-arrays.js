38f4ec50a6aebc04749ff325fa2ef8bd04d3296a
bebopmot <bebopmotinc@gmail.com>
2025-04-03 16:36:21 +0200

    functions-and-arrays solution

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,57 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+    if (a > b) {
+        return a;
+    }
+    else if (b > a) {
+        return b;
+    }
+    else if (a === b) {
+        return a && b;
+    }
+}
+maxOfTwoNumbers(156, 47);
+
+
+
 
 
 
 
 // Iteration 2 | Find the Longest Word
+
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+        return null;
+    }
 
+    let longestWord = "";
+
+    words.forEach(function(element) {
+        if (element.length > longestWord.length) {
+            longestWord = element;
+        }
+    });
+
+    return longestWord;
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+    let sum = 0;  
+
+    arr.forEach(function(element) {
+        sum += element;  
+    });
+
+    return sum;  
+}
 
 
 
@@ -23,12 +59,35 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
 
+function averageNumbers() {
+let sum = 0;  
+
+    arr.forEach(function(element) {
+        sum += element  
+    });
+
+    return sum;  
+}
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+    const  words = ""
+    arr.forEach(function(element) {
+         if (element === words){
+            return true
+           }  
+       else if (element !== words){
+        return false
+       } 
+       else if (element === "[]" ){
+        return "null"
+       }  
+    });
+}
+
+
